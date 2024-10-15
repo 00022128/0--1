bee305e1dce0f8f72d9df351fb95eb690916c378
T0PGcoder <aarifkhodjaev@students.wiut.uz>
2024-10-15 14:44:24 -0400

    Roll Dice Game

--- Randomsht/rolldicegame.py
+++ Randomsht/rolldicegame.py
@@ -41,7 +41,7 @@ num_of_dice = int(input("How many dice ?: "))
 for die in range(num_of_dice):
     dice.append(random.randint(1,6))
 
-for line in range(5g):
+for line in range(5):
     for die in dice:
         print(dice_art.get(die)[line], end="")
     print()
